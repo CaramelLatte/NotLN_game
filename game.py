2d1dd334169b34f8bd4419ffd4654cc017351c44
CaramelLatte <69405080+CaramelLatte@users.noreply.github.com>
2022-01-14 13:25:27 -0800

    Updated enemy spawns and stats

--- game.py
+++ game.py
@@ -11,13 +11,49 @@ def main():
   pygame.display.set_caption("Night of the Living Nerds")
   clock = pygame.time.Clock()
   font = pygame.font.Font(None, 50)
+  score = 0
 
   game_active = True
-
-  player_surf = pygame.Surface((20, 20))
-  player_rect = player_surf.get_rect(center = (512, 374))
   bullets = []
   enemies = []
+  i_frame = 0
+  is_hit = False
+
+  def spawn():
+    enemy_spawn = random.randint(0, 100)
+    if enemy_spawn > 1 and enemy_spawn < 5:
+      enemy_spawnx = 0
+      enemy_spawny = 0
+      spawn_wall = random.randint(1, 4)
+      match spawn_wall:
+        case 1:
+          enemy_spawnx = 50
+          enemy_spawny = random.randint(50, 718)
+        case 2:
+          enemy_spawnx = 984
+          enemy_spawny = random.randint(50, 718)
+        case 3:
+          enemy_spawny = 50
+          enemy_spawnx = random.randint(50, 984)
+        case 4:
+          enemy_spawny = 718
+          enemy_spawnx = random.randint(50, 984)
+
+      enemy_type = random.randint(1, 3)
+      match enemy_type:
+        case 1:
+          e = Enemy("white", enemy_spawnx, enemy_spawny, 15, 15, 1, 3)
+          enemies.append(e)
+          return
+        case 2:
+          e = Enemy("red", enemy_spawnx, enemy_spawny, 15, 15, 3, 1)
+          enemies.append(e)
+          return
+        case 3:
+          e = Enemy("Blue", enemy_spawnx, enemy_spawny, 15, 15, 2, 2)
+          enemies.append(e)
+          return         
+  
 
   class Entity:
     def __init__(self, color, x, y, width, height, speed, hp):
@@ -74,8 +110,8 @@ def main():
 
 
 
-  player = Entity("black", 100, 100, 20, 20, 5, 100 )
-  while True:
+  player = Entity("black", 100, 100, 20, 20, 5, 10 )
+  while game_active:
 
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
@@ -83,14 +119,22 @@ def main():
         exit()
       if event.type == pygame.MOUSEBUTTONDOWN:
         x,y = pygame.mouse.get_pos()
-        b = Bullet("black", player.rect.centerx, player.rect.centery, 5, 5, 10, 1, x, y)
+        b = Bullet("black", player.rect.centerx, player.rect.centery, 10, 10, 10, 1, x, y)
         bullets.append(b)
-        print(bullets)
     if game_active:
       screen.fill("#c0e8ec")
       player.draw(screen)
+      text_surface = font.render(f"Score: {score}", False, (64,64,64)).convert()
+      text_rectangle = text_surface.get_rect(center = (400, 50))
+      screen.blit(text_surface, text_rectangle)
+      pygame.draw.line(screen, 'black', player.rect.center, pygame.mouse.get_pos())
 
-    pygame.draw.line(screen, 'black', player.rect.center, pygame.mouse.get_pos())
+
+    if is_hit == True:
+      i_frame -= 1
+      if i_frame <= 0:
+        is_hit = False
+
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP] or keys[pygame.K_w]:
@@ -102,18 +146,30 @@ def main():
     if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
       player.rect.x += player.speed
 
-    enemy_spawn = random.randint(0, 100)
-    if enemy_spawn > 1 and enemy_spawn < 5:
-      e = Enemy("white", 0, 0, 15, 15, 2, 1)
-      enemies.append(e)
-
-
-    for i in reversed(range(len(bullets))):
-      for j in reversed(range(len(enemies))):
-        if bullets[i].collided(enemies[j].rect):
-          del enemies[j]
-          del bullets[i]
+    spawn()
+    #Check for bullet collision with enemies
+    for b in reversed(range(len(bullets))):
+      for e in reversed(range(len(enemies))):
+        if bullets[b].collided(enemies[e].rect):
+          del bullets[b]
+          enemies[e].hp -= 1
+          if enemies[e].hp <= 0:
+            del enemies[e]
+          score += 10
           break
+
+    for e in reversed(range(len(enemies))):
+      if player.collided(enemies[e].rect):
+        if i_frame <= 0 and is_hit == False:
+          del enemies[e]
+          player.hp -= 1
+          is_hit = True
+          i_frame = 15
+          print(f"Player HP remaining: {player.hp}")
+          if player.hp <= 0:
+            game_active = False
+
+
     for b in bullets:
       b.move()
       if b.x < 0 or b.x > 1024:
@@ -129,11 +185,6 @@ def main():
     for e in enemies:
       e.draw(screen)
 
-
-    
-    for e in enemies:
-      if e.collided(player.rect):
-        print("Got hit!")
       
     pygame.display.update()
     clock.tick(60)
