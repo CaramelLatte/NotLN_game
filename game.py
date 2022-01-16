c3e4946c9e51985c9c84a515e32995aa015926a3
CaramelLatte <69405080+CaramelLatte@users.noreply.github.com>
2022-01-16 13:50:17 -0800

    more updates

--- game.py
+++ game.py
@@ -28,15 +28,15 @@ def main():
       match spawn_wall:
         case 1:
           enemy_spawnx = 50
-          enemy_spawny = random.randint(50, 718)
+          enemy_spawny = random.randint(50, 568)
         case 2:
           enemy_spawnx = 984
-          enemy_spawny = random.randint(50, 718)
+          enemy_spawny = random.randint(50, 568)
         case 3:
           enemy_spawny = 50
           enemy_spawnx = random.randint(50, 984)
         case 4:
-          enemy_spawny = 718
+          enemy_spawny = 568
           enemy_spawnx = random.randint(50, 984)
 
       enemy_type = random.randint(1, 3)
@@ -50,7 +50,7 @@ def main():
           enemies.append(e)
           return
         case 3:
-          e = Enemy("Blue", enemy_spawnx, enemy_spawny, 15, 15, 2, 2)
+          e = Enemy("blue", enemy_spawnx, enemy_spawny, 15, 15, 2, 2)
           enemies.append(e)
           return         
   
@@ -63,11 +63,11 @@ def main():
       self.hp = hp
       self.x = x
       self.y = y
+      self.fire_rate = 1
+      self.shot_size = 1
+      self.damage = 1
+      self.spray = False
 
-    def move(self):
-      pass
-    def moveDirection(self):
-      pass
     def collided(self, rect):
       return self.rect.colliderect(rect)
     def draw(self, surface):
@@ -111,15 +111,12 @@ def main():
 
 
   player = Entity("black", 100, 100, 20, 20, 5, 10 )
+  shot_cd = 0
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         pygame.quit()
         exit()
-      if event.type == pygame.MOUSEBUTTONDOWN:
-        x,y = pygame.mouse.get_pos()
-        b = Bullet("black", player.rect.centerx, player.rect.centery, 10, 10, 10, 1, x, y)
-        bullets.append(b)
       if event.type == pygame.KEYDOWN and game_active == False:
         if event.key == pygame.K_SPACE:
           game_active = True
@@ -129,38 +126,94 @@ def main():
             del enemies[e]
         for b in reversed(range(len(bullets))):
             del bullets[b]
+      if event.type == pygame.MOUSEBUTTONDOWN:
+        #box 1
+        if ui_1.collidepoint(event.pos):
+          if player.fire_rate <= 2:
+            player.fire_rate += 1
+          else:
+            player.fire_rate = 1
+
+        if ui_2.collidepoint(event.pos):
+          if player.shot_size <= 2:
+            player.shot_size += 1
+          else:
+            player.shot_size = 1
+
+        if ui_3.collidepoint(event.pos):
+          if player.damage <= 2:
+            player.damage += 1
+          else:
+            player.damage = 1
+        
+        if ui_4.collidepoint(event.pos):
+          player.spray = not player.spray
+          print(player.spray)
+        
+        
+
     if game_active:
 
         screen.fill("#c0e8ec")
         player.draw(screen)
         text_surface = font.render(f"Score: {score}", False, (64,64,64)).convert()
         text_rectangle = text_surface.get_rect(center = (screen.get_width() /2, 50))
+        ui_rectangle = pygame.Rect(0, 618, 1024, 150) 
+        pygame.draw.rect(screen, "white", ui_rectangle)
+        ui_1 = pygame.Rect(0, 618, 256, 150)
+        pygame.draw.rect(screen, "Blue", ui_1)
+        pygame.draw.rect(screen, "Black", ui_1, 1)
+        ui_2 = pygame.Rect(256, 618, 256, 150)
+        pygame.draw.rect(screen, "Blue", ui_2)
+        pygame.draw.rect(screen, "Black", ui_2, 1)
+        ui_3 = pygame.Rect(512, 618, 256, 150)
+        pygame.draw.rect(screen, "Blue", ui_3)
+        pygame.draw.rect(screen, "Black", ui_3, 1)
+        ui_4 = pygame.Rect(768, 618, 256, 150)
+        pygame.draw.rect(screen, "Blue", ui_4)
+        pygame.draw.rect(screen, "Black", ui_4, 1)
+
         screen.blit(text_surface, text_rectangle)
         pygame.draw.line(screen, 'black', player.rect.center, pygame.mouse.get_pos())
 
 
-        if is_hit == True:
-          i_frame -= 1
-          if i_frame <= 0:
-            is_hit = False
-
+        i_frame -= 1
+        if i_frame <= 0:
+          is_hit = False
+        if shot_cd > 0:
+          shot_cd -= player.fire_rate
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP] or keys[pygame.K_w]:
-          player.rect.y -= player.speed
-        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-          player.rect.y += player.speed
-        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-          player.rect.x -= player.speed
-        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-          player.rect.x += player.speed
+        player.rect.x += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * player.speed
+        if player.rect.x <= 0:
+          player.rect.x = 0
+        if player.rect.x >= 1004:
+          player.rect.x = 1004
+        player.rect.y += (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * player.speed
+        if player.rect.y <= 0:
+          player.rect.y = 0
+        if player.rect.y >= 598:
+          player.rect.y = 598
+
+        if pygame.mouse.get_pressed()[0] and shot_cd <= 0:
+          shot_cd = 20
+          x,y = pygame.mouse.get_pos()
+          b = Bullet("black", player.rect.centerx, player.rect.centery, (5 * player.shot_size), (5 * player.shot_size), 10, 1, x, y)
+          bullets.append(b)
+          if player.spray == True:
+            b1 = Bullet("black", player.rect.centerx, player.rect.centery, (5 * player.shot_size), (5 * player.shot_size), 10, 1, x + 30, y + 30)
+            b2 = Bullet("black", player.rect.centerx, player.rect.centery, (5 * player.shot_size), (5 * player.shot_size), 10, 1, x - 30, y - 30)
+            bullets.append(b1)
+            bullets.append(b2)
+        
+
 
         spawn_enemies()
         for b in reversed(range(len(bullets))):
           for e in reversed(range(len(enemies))):
             if bullets[b].collided(enemies[e].rect):
               del bullets[b]
-              enemies[e].hp -= 1
+              enemies[e].hp -= 1 * player.damage
               if enemies[e].hp <= 0:
                 del enemies[e]
               score += 10
@@ -173,16 +226,16 @@ def main():
               player.hp -= 1
               is_hit = True
               i_frame = 15
-              print(f"Player HP remaining: {player.hp}")
+              #print(f"Player HP remaining: {player.hp}")
               if player.hp <= 0:
                 game_active = False
 
-        for b in bullets:
-          b.move()
-          if b.x <= -50 or b.x >= 1074:
-            bullets.remove(b)
-          if b.y <= -50 or b.y > 818:
-            bullets.remove(b)
+        for b in reversed(range(len(bullets))):
+          bullets[b].move()
+          if bullets[b].x <= -50 or bullets[b].x >= 1074:
+            del bullets[b]
+          elif bullets[b].y <= -50 or bullets[b].rect.midbottom[1] >= 618:
+            del bullets[b]
 
         for e in enemies:
           e.move()
